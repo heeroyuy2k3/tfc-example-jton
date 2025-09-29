06597679d564a12beab0a4aeaecc70d3e908b9a9
heeroyuy2k3 <119994957+heeroyuy2k3@users.noreply.github.com>
2025-09-29 15:51:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 # Create a resource group using the generated random name
@@ -25,32 +28,3 @@ resource "azurerm_resource_group" "example" {
   location = "eastus"
   name     = "MITS-TF-RG"
 }
-
-#provider "aws" {
-#  region = var.region
-#}
-#
-#data "aws_ami" "ubuntu" {
-#  most_recent = true
-#
-#  filter {
-#    name   = "name"
-#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#  }
-#
-#  filter {
-#    name   = "virtualization-type"
-#    values = ["hvm"]
-#  }
-#
-#  owners = ["099720109477"] # Canonical
-#}
-#
-#resource "aws_instance" "ubuntu" {
-#  ami           = data.aws_ami.ubuntu.id
-#  instance_type = var.instance_type
-#
-#  tags = {
-#    Name = var.instance_name
-#  }
-#}
