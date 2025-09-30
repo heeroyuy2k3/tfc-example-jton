9c6e5f174bd2a40c0f45ac820a584fab0e38e196
heeroyuy2k3 <119994957+heeroyuy2k3@users.noreply.github.com>
2025-09-30 14:14:57 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "hello_world" {
   value       = "Hello World!"
 }
 
- "resource_group_name" {
+output "resource_group_name" {
   description = "The name of the resource group"
   value       = azurerm_resource_group.rg.name
 }
