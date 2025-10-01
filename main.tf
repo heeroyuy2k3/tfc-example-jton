2d5858fc75be8be1dc406b9f5098250d4c7b5530
heeroyuy2k3 <119994957+heeroyuy2k3@users.noreply.github.com>
2025-10-01 18:23:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "azurerm_network_interface" "vm_nic" {
 
 # 7. CIS Windows Server 2022 Level 1 Generation 2
 resource "azurerm_virtual_machine" "cisvm" {
-  name                  = "cis-l1-vm"
+  name                  = "cis-ws2022-l1g2-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vm_nic.id]
@@ -84,15 +84,15 @@ resource "azurerm_virtual_machine" "cisvm" {
   # Use the CIS hardened image from Marketplace
   storage_image_reference {
     publisher = "center-for-internet-security-inc"
-    offer     = "cis-windows-server"      # example: CIS Windows Server 2022 Level 1 Generation 2 offer
-    sku       = "cis-windows-server2022-l1-gen2"                # SKU for the level 1 gen 2 image
+    offer     = "cis-windows-server-2022-l1"      # example: CIS Windows Server 2022 Level 1 Generation 2 offer
+    sku       = "cis-windows-server-2022-l1-gen2"                # SKU for the level 1 gen 2 image
     version   = "latest"
   }
 
   # Required plan block for marketplace images with licensing/terms
   plan {
-    name      = "cis-ws2022-l1g2-vm01"
-    product   = "cis-windows-server2022-l1-gen2"
+    name      = "cis-windows-server-2022-l1-gen2"
+    product   = "cis-windows-server-2022-l1"
     publisher = "center-for-internet-security-inc"
   }
 
